d7f82ee1fd948647cc68e278d0b803996db0b2bb
devYuMinKim <gimyumin40@gmail.com>
2023-09-29 22:04:24 +0900

    feat: Implement error page

--- next.js13-tailwindcss-blog/src/blogAPI.ts
+++ next.js13-tailwindcss-blog/src/blogAPI.ts
@@ -3,6 +3,10 @@ import { Article } from "./types";
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" }); // SSR. force-cache: SSG, default. next: { revalidate: 10 }: ISR.
 
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。"); // エラー処理
+  }
+
   const articles = await res.json(); // 文字列化
   return articles;
 };
