25dd0265a58c95cefc1145bde2017d7a24a2e58c
devYuMinKim <gimyumin40@gmail.com>
2023-09-29 14:53:55 +0900

    style: Implement ArticleList page layout

--- next.js13-tailwindcss-blog/src/app/components/ArticleList.tsx
+++ next.js13-tailwindcss-blog/src/app/components/ArticleList.tsx
@@ -1,7 +1,74 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ArticleList = () => {
-  return <div>ArticleList</div>;
+  return (
+    <div>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2023/09/29</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
+            accusamus dolore maxime praesentium soluta expedita architecto
+            porro, quis autem doloribus labore nam, laborum eaque, dolores natus
+            rem laboriosam voluptate non!
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2023/09/29</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
+            accusamus dolore maxime praesentium soluta expedita architecto
+            porro, quis autem doloribus labore nam, laborum eaque, dolores natus
+            rem laboriosam voluptate non!
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default ArticleList;
