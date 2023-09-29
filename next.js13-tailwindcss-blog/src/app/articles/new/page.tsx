511e4954372bbd72ebdd178c63412bba78ef2ba9
devYuMinKim <gimyumin40@gmail.com>
2023-09-30 01:44:43 +0900

    feat: Implement posting API

--- next.js13-tailwindcss-blog/src/app/articles/new/page.tsx
+++ next.js13-tailwindcss-blog/src/app/articles/new/page.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+"use client";
+
+import { createArticle } from "@/blogAPI";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const CreateBlogPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    await createArticle(id, title, content);
+
+    setLoading(false);
+    router.push("/");
+    router.refresh();
+  };
+
   return (
     <div className="min-h-screen py-8 px-4 md:px-12">
       <h2 className="text-2xl font-bold mb-4">ブログ新規作成</h2>
-      <form className="bg-slate-200 p-6 rounded shadow-lg">
+      <form
+        className="bg-slate-200 p-6 rounded shadow-lg"
+        onSubmit={handleSubmit}
+      >
         <div className="text-gray-700 text-sm font-bold mb-2">
           <label>URL</label>
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setId(e.target.value)}
           />
         </div>
         <div className="text-gray-700 text-sm font-bold mb-2">
@@ -17,16 +43,25 @@ const CreateBlogPage = () => {
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="text-gray-700 text-sm font-bold mb-2">
           <label>本文</label>
-          <textarea className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" />
+          <textarea
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
 
         <button
           type="submit"
-          className="py-2 px-4 border rounded-md bg-orange-300"
+          className={`py-2 px-4 border rounded-md ${
+            loading
+              ? "bg-orange-300 cursor-not-allowed"
+              : "bg-orange-400 hover:bg-orange-500"
+          }"}`}
+          disabled={loading}
         >
           投稿
         </button>
--- next.js13-tailwindcss-blog/src/blogAPI.ts
+++ next.js13-tailwindcss-blog/src/blogAPI.ts
@@ -32,3 +32,28 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json(); // 文字列化
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDatetime = new Date().toISOString();
+
+  const res = await fetch(`http://localhost:3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, createdAt: currentDatetime }),
+  }); // ISR
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。"); // エラー処理
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500)); // 1.5秒待つ
+
+  const newArticle = await res.json(); // 文字列化
+  return newArticle;
+};
