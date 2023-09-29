c3cb9964f7865aebec0a84f870a1b2be07f79c74
devYuMinKim <gimyumin40@gmail.com>
2023-09-29 11:37:50 +0900

    feat: Implement footer

---
+++ next.js13-tailwindcss-blog/src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-2 px-4 border-t">
+      <small>@2023 yuminn-k</small>
+    </footer>
+  );
+};
+
+export default Footer;
