b390eee126d98e4b0886249908fa279c33e6a531
devYuMinKim <gimyumin40@gmail.com>
2023-09-30 00:34:44 +0900

    feat: Implement ArticleCard page

--- next.js13-tailwindcss-blog/src/app/articles/[id]/page.tsx
+++ next.js13-tailwindcss-blog/src/app/articles/[id]/page.tsx
@@ -1,18 +1,23 @@
+import { getDetailArticle } from "@/blogAPI";
 import Image from "next/image";
 import React from "react";
 
-const Article = ({ params }: { params: { id: string } }) => {
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticle(params.id);
+
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}}`}
         alt=""
         width={1280}
         height={300}
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">ここがタイトルです。</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        <p>ここが本文です。</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   );
--- next.js13-tailwindcss-blog/src/blogAPI.ts
+++ next.js13-tailwindcss-blog/src/blogAPI.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
@@ -11,4 +12,23 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json(); // 文字列化
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); // ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。"); // エラー処理
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500)); // 1.5秒待つ
+
+  const article = await res.json(); // 文字列化
+  return article;
 };
